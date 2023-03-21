365494a6b5eab8fc24c50d2fcb8a0c5a95e2a859
Ebolak <ebolak@dztsound.com>
2023-03-21 07:50:00 +0100

    App run on local host, no debug and reloader

--- app.py
+++ app.py
@@ -243,4 +243,5 @@ def handle_disconnect():
 
 if __name__ == "__main__":
     Base.metadata.create_all(bind=engine)
-    socketio.run(app, host="0.0.0.0", port=5000, use_reloader=True, debug=True)
+    socketio.run(app, host="127.0.0.1", port=5000,
+                 use_reloader=False, debug=False)
